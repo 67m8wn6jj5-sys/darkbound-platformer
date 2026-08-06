caa3bc99ceb16eaa9af21eface46e7bee9e82916
67m8wn6jj5-sys <67m8wn6jj5@privaterelay.appleid.com>
2026-08-06 12:26:03 -0400

    Fix touch-only startup on Safari

--- src/InputManager.js
+++ src/InputManager.js
@@ -1,38 +1,45 @@
 export class InputManager {
   constructor(scene) {
     this.scene = scene;
-    this.keys = scene.input.keyboard.addKeys({
+    this.keys = scene.input.keyboard?.addKeys({
       left:'A', right:'D', left2:'LEFT', right2:'RIGHT',
       jump:'SPACE', jump2:'W', jump3:'UP', dodge:'SHIFT', dodge2:'K', pause:'ESC'
-    });
+    }) ?? {};
     this.touch = { left:false, right:false, jumpPressed:false, jumpHeld:false, dodgePressed:false };
-    this.lastSource = 'Keyboard';
+    this.lastSource = 'Touch';
     this.pad = null;
 
-    // Safari may not expose the gamepad plugin until after a user gesture.
-    // Guard every access so touch play still starts normally.
-    if (scene.input.gamepad) {
-      scene.input.gamepad.once('connected', pad => {
+    const gamepadPlugin = scene.input.gamepad;
+    if (gamepadPlugin) {
+      gamepadPlugin.once('connected', pad => {
         this.pad = pad;
         this.lastSource = 'Controller';
       });
     }
   }
 
+  keyDown(name) {
+    return !!this.keys?.[name]?.isDown;
+  }
+
+  keyPressed(name) {
+    const key = this.keys?.[name];
+    return !!key && Phaser.Input.Keyboard.JustDown(key);
+  }
+
   update() {
     const gamepadPlugin = this.scene.input.gamepad;
     if (!this.pad && gamepadPlugin?.total) this.pad = gamepadPlugin.getPad(0);
 
-    const k = this.keys;
     const axis0 = this.pad?.axes?.[0];
     const rawAxis = axis0?.getValue?.() ?? 0;
     const padAxis = Math.abs(rawAxis) > .18 ? rawAxis : 0;
     const padLeft = !!this.pad?.left || padAxis < -.18;
     const padRight = !!this.pad?.right || padAxis > .18;
-    const keyboardLeft = k.left.isDown || k.left2.isDown;
-    const keyboardRight = k.right.isDown || k.right2.isDown;
+    const keyboardLeft = this.keyDown('left') || this.keyDown('left2');
+    const keyboardRight = this.keyDown('right') || this.keyDown('right2');
 
-    if (keyboardLeft || keyboardRight || Phaser.Input.Keyboard.JustDown(k.jump) || Phaser.Input.Keyboard.JustDown(k.dodge)) this.lastSource='Keyboard';
+    if (keyboardLeft || keyboardRight || this.keyPressed('jump') || this.keyPressed('dodge')) this.lastSource='Keyboard';
     if (padLeft || padRight || this.pad?.A || this.pad?.B) this.lastSource='Controller';
     if (this.touch.left || this.touch.right || this.touch.jumpHeld || this.touch.dodgePressed) this.lastSource='Touch';
 
@@ -41,18 +48,18 @@ export class InputManager {
     if (keyboardRight || padRight || this.touch.right) move += 1;
     move = Phaser.Math.Clamp(move + padAxis, -1, 1);
 
-    const jumpPressed = Phaser.Input.Keyboard.JustDown(k.jump)
-      || Phaser.Input.Keyboard.JustDown(k.jump2)
-      || Phaser.Input.Keyboard.JustDown(k.jump3)
+    const jumpPressed = this.keyPressed('jump')
+      || this.keyPressed('jump2')
+      || this.keyPressed('jump3')
       || (!!this.pad?.A && !this._padA)
       || this.touch.jumpPressed;
-    const jumpHeld = k.jump.isDown || k.jump2.isDown || k.jump3.isDown || !!this.pad?.A || this.touch.jumpHeld;
-    const dodgePressed = Phaser.Input.Keyboard.JustDown(k.dodge)
-      || Phaser.Input.Keyboard.JustDown(k.dodge2)
+    const jumpHeld = this.keyDown('jump') || this.keyDown('jump2') || this.keyDown('jump3') || !!this.pad?.A || this.touch.jumpHeld;
+    const dodgePressed = this.keyPressed('dodge')
+      || this.keyPressed('dodge2')
       || (!!this.pad?.B && !this._padB)
       || this.touch.dodgePressed;
     const startPressed = !!this.pad?.buttons?.[9]?.pressed;
-    const pausePressed = Phaser.Input.Keyboard.JustDown(k.pause) || (startPressed && !this._padStart);
+    const pausePressed = this.keyPressed('pause') || (startPressed && !this._padStart);
 
     this._padA=!!this.pad?.A;
     this._padB=!!this.pad?.B;
